e2fb16f59304448f92c505935ef8f395f12e90be
Sahilitrix <sahilitrix123@gmail.com>
2020-05-20 13:55:52 -0400

    create boolean_test.dart

---
+++ android/app/src/main/kotlin/com/example/flutter_sandbox/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_sandbox
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ test/boolean_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+void main() {
+  group('boolean', () {
+    test('Test should result in true', () {
+      final val = true;
+      expect(val, isTrue);
+    });
+    test('Test should be false', () {
+      final val = false;
+      expect(val, false);
+    });
+  });
+}
