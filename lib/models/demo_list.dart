d67b95a9a93d17b6fbcc84bdc133233b13f5ace7
sahitpen <sahilitrix123@gmail.com>
2020-05-27 13:55:48 -0400

    remove duplicate demo

--- lib/models/demo_list.dart
+++ lib/models/demo_list.dart
@@ -1,13 +1,13 @@
 import 'package:flutter_sandbox/screens/dismissible_page.dart';
+import 'package:flutter_sandbox/screens/draggable_page.dart';
 
 import 'demo.dart';
 
 class DemoList{
   static final touchInteractionDemos = [
     Demo(name: 'Dismissible', pageRoute: DismissiblePage()),
-    Demo(name: 'Draggable', pageRoute: null),
+    Demo(name: 'Draggable', pageRoute: DraggablePage()),
     Demo(name: 'Scrollable', pageRoute: null),
-    Demo(name: 'Long Press Draggable', pageRoute: null)
   ];
   static final inputDemos = [
     Demo(name: 'Form', pageRoute: null),
