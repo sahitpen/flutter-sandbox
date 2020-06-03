5bc04148edea466c40539fb007ea49d6bee6cb79
sahitpen <sahilitrix123@gmail.com>
2020-06-03 11:22:35 -0400

    convert homepage to stateless widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -4,16 +4,11 @@ import 'package:flutter_sandbox/views/appbar.dart';
 import 'package:flutter_sandbox/views/demo_elements/demo_list_container.dart';
 import 'package:flutter_sandbox/views/navbar.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({
     Key key,
   }) : super(key: key);
 
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final _theme = AppTheme.theme;
