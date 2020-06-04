078d59dbf35aa37db14b0da2e2383785223c52de
sahitpen <sahilitrix123@gmail.com>
2020-06-04 09:55:20 -0400

    change formatting

--- lib/bloc/dropdown_event.dart
+++ lib/bloc/dropdown_event.dart
@@ -5,5 +5,8 @@ abstract class DropdownEvent {}
 
 class SelectItem extends DropdownEvent {
   final String item;
-  SelectItem(this.item) : assert(item != null);
+  SelectItem(this.item)
+      : assert(
+          item != null,
+        );
 }
